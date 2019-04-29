b454a196e91035c667164a19f10fe6450f425add
David Buick <buickzmyname@gmail.com>
2019-04-29 14:46:32 +1200

    added crew and spaceship

---
+++ Game/src/main/Crew.java
@@ -0,0 +1,53 @@
+package main;
+
+public class Crew {
+	private int health;
+	private int shieldRepairValue;
+	private int searchProficiency;
+	private String name;
+	private int tiredness;
+	boolean sick;
+	boolean piolting;
+	
+	public String viewStatus(){
+		return;
+	}
+	
+	public void repair(){
+		return;
+	}
+	
+	public void search(){
+		return;
+	}
+	
+	public void pilot(){
+		return;
+	}
+	
+	public void sleep(){
+		return;
+	}
+	
+	public void consumeItem(){
+		return;
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+	
+	public void setHealth(int value){
+		this.health = value;
+		return;
+	}
+	
+	public int  getFatigue(){
+		return tiredness;
+	}
+	
+	public void setFatigue(int value){
+		this.tiredness = value;
+		return;
+	}
+}
---
+++ Game/src/main/Spaceship.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Spaceship {
+	private ArrayList<Crew> crew;
+	private ArrayList<Item> inventory;
+	private int shield;
+	private int money;
+	
+	public void viewCrew(){
+		return;
+	}
+	
+	public void viewInventory(){
+		return;
+	}
+	
+	public int viewMoney(){
+		return money;
+	}
+	
+	public int viewShield(){
+		return shield;
+	}
+	
+	public void addItem(Item item){
+		return;
+	}
+	
+	public void removeItem(Item item){
+		return;
+	}
+}
