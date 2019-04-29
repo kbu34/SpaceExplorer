619c951b1a5d9d37959d3c3d586157dfa46cc591
David Buick <buickzmyname@gmail.com>
2019-04-29 14:06:46 +1200

    added foods

--- Game/src/main/Item.java
+++ Game/src/main/Item.java
@@ -2,4 +2,35 @@ package main;
 
 public class Item {
 	
+	private String name;
+	private int value;
+	
+	public Item(String name, int value){
+		this.name = name;
+		this.value = value;
+	}
+	
+	public void useItem(){
+		return;
+	}
+	
+	public void buyItem(){
+		return;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
+	
+	public void setValue(int newValue){
+		this.value = newValue;
+	}
+	
+	public String toString(){
+		return name;
+	}
 }
