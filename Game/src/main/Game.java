3571586399959222a6297be3eea8705a75d54575
Phillip Kim <kbu34@uclive.co.nz>
2019-05-05 16:20:07 +1200

    Game and TransporterPiece classes added

---
+++ Game/src/main/Game.java
@@ -0,0 +1,34 @@
+package main;
+
+public class Game {
+	private int spaceshipLocation;
+	private int gameLength;
+	private int turnNum;
+	private int totalParts;
+	private int partsAquired;
+	
+	public void startGame() {
+		return;
+	}
+	
+	public void nextTurn() {
+		return;
+	}
+	
+	public void eventPicker() {
+		return;
+	}
+	
+	public void astroidBelt() {
+		return;
+	}
+	
+	public void pirates() {
+		return;
+	}
+	
+	public void plague() {
+		return;
+	}
+
+}
